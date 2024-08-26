1bd6045d028f8eeb8bc31afc83df46211a0263b1
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-08-26 17:43:10 -0500

    Added rules and members

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { Confetti } from 'svelte-confetti';
-	import { Button } from '$lib/components/ui/button';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { onMount } from 'svelte';
+	import Info from 'lucide-svelte/icons/info';
+	import Book from 'lucide-svelte/icons/book';
 
 	function preventDoubleClick(event) {
 		event.preventDefault();
@@ -27,7 +30,11 @@
 	const questions = [
 		{ question: 'Cual empanada es mejor?', leftAnswer: 'Saudade', rightAnswer: 'Típicas' },
 		{ question: 'Tener clase en ...', leftAnswer: 'El SD', rightAnswer: 'El O' },
-
+		{
+			question: 'Hacer trabajos en...',
+			leftAnswer: 'Biblioteca del ML',
+			rightAnswer: 'Biblioteca del SD'
+		},
 		{ question: 'Para tu clase vas por:', leftAnswer: 'Ascensor', rightAnswer: 'Escaleras' },
 		{ question: 'En el salón te haces:', leftAnswer: 'Adelante', rightAnswer: 'Atrás' },
 		{ question: 'Llegas a la U en:', leftAnswer: 'Wheels', rightAnswer: 'Transmi' },
@@ -47,10 +54,10 @@
 	let rightAngle = rightAngleDegrees * (Math.PI / 180); // Convert to radians
 
 	// List of prime numbers between 0 and 255
-	const primeNumbers = [
-		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-		101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-		197, 199, 211, 223, 227, 229, 233, 239, 241, 251
+	const numbers = [
+		10, 27, 28, 51, 53, 74, 74, 88, 95, 139, 141, 143, 152, 153, 158, 172, 174, 177, 187, 200, 201,
+		212, 214, 248, 254, 258, 278, 280, 289, 296, 307, 343, 345, 347, 359, 363, 377, 384, 421, 427,
+		451, 468, 474, 483, 488, 499, 504, 506, 509, 512
 	];
 
 	// Function to generate a random color in hex format
@@ -116,9 +123,9 @@
 			// End of questions
 			const binaryValue = calculateBinaryPath(currentPath);
 			finalMessage = `Camino: ${binaryValue}`;
-			if (binaryValue === 42) {
+			if (binaryValue === 314) {
 				prizeMessage = 'gran';
-			} else if (primeNumbers.includes(binaryValue)) {
+			} else if (numbers.includes(binaryValue)) {
 				prizeMessage = 'peq';
 			} else {
 				prizeMessage = ''; // No prize if not a prime number or 42
@@ -146,11 +153,11 @@
 
 <!-- Main container -->
 <div
-	class="flex h-dvh touch-pan-x select-none flex-col items-center justify-center p-6 font-mono"
+	class="flex h-dvh touch-pan-x select-none flex-col items-center justify-center p-6 font-mono text-white"
 	style={gradientStyle}
 >
 	<!-- Game Title -->
-	<h1 class="font-custom mb-8 text-4xl text-white">Búsqueda Fractal</h1>
+	<h1 class="font-custom mb-8 text-4xl">Búsqueda Fractal</h1>
 
 	<!-- SVG to render the tree -->
 	<div class="relative w-full max-w-screen-md grow overflow-hidden">
@@ -168,24 +175,62 @@
 			{/each}
 		</svg>
 	</div>
+	<Dialog.Root>
+		<Dialog.Trigger class="absolute bottom-4 right-4"><Book class="h-6 w-6" /></Dialog.Trigger>
+		<Dialog.Content class=" rounded-xl sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>Reglas del Juego</Dialog.Title>
+				<Dialog.Description class="flex flex-col justify-start text-left">
+					<strong>Pregunta:</strong> Responde a cada pregunta eligiendo entre las opciones
+					proporcionadas.
+
+					<strong>Elección:</strong> Cada respuesta te llevará por un camino diferente en el árbol,
+					la respuesta que elijas determina la direccion de la rama.
+
+					<strong>Premios:</strong> Algunos caminos tienen un premio al final, si nadie más lo ha
+					encontrado es tuyo.
+
+					<strong>Reinicio:</strong> Intenta jugar muchas veces y ver si se forma un patron en el arbol.
+				</Dialog.Description>
+			</Dialog.Header>
+			<Dialog.Footer>
+				<Button type="button">Cerrar</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
+
+	<Dialog.Root>
+		<Dialog.Trigger class="absolute bottom-4 left-4"><Info class="h-6 w-6" /></Dialog.Trigger>
+		<Dialog.Content class="rounded-xl sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>Grupo H - Juegos y Complejidad</Dialog.Title>
+				<Dialog.Description class="text-left">
+					<p>Daniela Espinosa</p>
+					<p>Sofía Torres</p>
+					<p>Mario Alejandro Ruiz</p>
+					<p>Sofia Bello</p>
+					<p>Diego Chacon</p>
+					<p class="mt-4"><strong>Universidad de los Andes 2024</strong></p>
+				</Dialog.Description>
+			</Dialog.Header>
+			<Dialog.Footer>
+				<Button type="button">Cerrar</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
+
 	<!-- Display questions and buttons -->
 	<div class="h-44">
 		{#if currentQuestionIndex >= 0}
 			<div class=" text-center text-2xl">
-				<p class="mb-4 font-semibold text-white">
+				<p class="mb-4 font-semibold">
 					{questions[currentQuestionIndex].question}
 				</p>
 				<div class="flex justify-center gap-4">
-					<Button
-						on:click={() => nextQuestion('L')}
-						class="bg-emerald-500 text-white hover:bg-emerald-600"
-					>
+					<Button on:click={() => nextQuestion('L')} class="bg-emerald-500  hover:bg-emerald-600">
 						{questions[currentQuestionIndex].leftAnswer}
 					</Button>
-					<Button
-						on:click={() => nextQuestion('R')}
-						class="bg-amber-500 text-white hover:bg-amber-600"
-					>
+					<Button on:click={() => nextQuestion('R')} class="bg-amber-500  hover:bg-amber-600">
 						{questions[currentQuestionIndex].rightAnswer}
 					</Button>
 				</div>
