b6752506e3e95eb2fd6a9a23d45a880df65ec2a7
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-08-26 14:22:45 -0500

    Reduced set

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,11 +27,7 @@
 	const questions = [
 		{ question: 'Cual empanada es mejor?', leftAnswer: 'Saudade', rightAnswer: 'Típicas' },
 		{ question: 'Tener clase en ...', leftAnswer: 'El SD', rightAnswer: 'El O' },
-		{
-			question: 'Hacer trabajos en...',
-			leftAnswer: 'Biblioteca del ML',
-			rightAnswer: 'Biblioteca del SD'
-		},
+
 		{ question: 'Para tu clase vas por:', leftAnswer: 'Ascensor', rightAnswer: 'Escaleras' },
 		{ question: 'En el salón te haces:', leftAnswer: 'Adelante', rightAnswer: 'Atrás' },
 		{ question: 'Llegas a la U en:', leftAnswer: 'Wheels', rightAnswer: 'Transmi' },
@@ -54,9 +50,7 @@
 	const primeNumbers = [
 		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
 		101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-		197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
-		311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421,
-		431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509
+		197, 199, 211, 223, 227, 229, 233, 239, 241, 251
 	];
 
 	// Function to generate a random color in hex format
